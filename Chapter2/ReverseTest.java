c0bca588196a03e33e09c97ef17db603fc1a68c9
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-01-02 03:10:49 +0545

    removed the ip address

--- Chapter2/ReverseTest.java
+++ Chapter2/ReverseTest.java
@@ -5,7 +5,7 @@ import java.net.UnknownHostException;
 
 public class ReverseTest {
     public static void main(String[] args) throws UnknownHostException {
-        InetAddress ip = InetAddress.getByName("104.21.79.8");
+        InetAddress ip = InetAddress.getByName("XXX.XXX.XXX.XXX");
         System.out.println("Host : " + ip.getCanonicalHostName());
     }
 }
