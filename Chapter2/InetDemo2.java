97c96a48667ecb0b0a145af6f332875db3724a46
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-01-02 02:39:13 +0545

    inet demo 2 added

---
+++ Chapter2/InetDemo2.java
@@ -0,0 +1,31 @@
+package Chapter2;
+
+import java.net.Inet4Address;
+import java.util.Arrays;
+import java.net.InetAddress;
+
+public class InetDemo2 {
+    public static void main(String[] arg) throws Exception {
+        InetAddress ip = Inet4Address.getByName("chandanbhagat.com.np");
+        InetAddress ip1[] = InetAddress.getAllByName("chandanbhagat.com.np");
+        byte addr[] = { 72, 3, 2, 12 };
+        System.out.println("ip : " + ip);
+        System.out.print("\nip1 : " + ip1);
+        InetAddress ip2 = InetAddress.getByAddress(addr);
+        System.out.print("\nip2 : " + ip2);
+        System.out.print("\nAddress : " + Arrays.toString(ip.getAddress()));
+        System.out.print("\nHost Address : " + ip.getHostAddress());
+        System.out.print("\nisAnyLocalAddress : " + ip.isAnyLocalAddress());
+        System.out.print("\nisLinkLocalAddress : " + ip.isLinkLocalAddress());
+        System.out.print("\nisLoopbackAddress : " + ip.isLoopbackAddress());
+        System.out.print("\nisMCGlobal : " + ip.isMCGlobal());
+        System.out.print("\nisMCLinkLocal : " + ip.isMCLinkLocal());
+        System.out.print("\nisMCNodeLocal : " + ip.isMCNodeLocal());
+        System.out.print("\nisMCOrgLocal : " + ip.isMCOrgLocal());
+        System.out.print("\nisMCSiteLocal : " + ip.isMCSiteLocal());
+        System.out.print("\nisMulticastAddress : " + ip.isMulticastAddress());
+        System.out.print("\nisSiteLocalAddress : " + ip.isSiteLocalAddress());
+        System.out.print("\nhashCode : " + ip.hashCode());
+        System.out.print("\n Is ip1 == ip2 : " + ip.equals(ip2));
+    }
+}
