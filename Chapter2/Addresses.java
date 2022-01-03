012ce3ef94a46a832d8a8427772562f9d537f213
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-01-03 05:58:28 +0545

    address test code added

---
+++ Chapter2/Addresses.java
@@ -0,0 +1,18 @@
+package Chapter2;
+
+import java.net.InetAddress;
+
+public class Addresses {
+    public static void main(String[] args) throws Exception {
+        InetAddress ip = InetAddress.getLocalHost();
+        byte[] bytes = ip.getAddress();
+        if (bytes.length == 4) {
+            System.out.println("IPv4");
+        } else if (bytes.length == 16) {
+            System.out.println("IPv6");
+        } else {
+            System.out.println("Neither");
+        }
+
+    }
+}
