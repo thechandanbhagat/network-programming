069fe960bd95d9be922c2dd46005788e0c2c5f90
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-01-03 06:04:05 +0545

    localhost address code added

---
+++ Chapter2/Localhost.java
@@ -0,0 +1,13 @@
+package Chapter2;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Localhost {
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress ip = InetAddress.getLocalHost();
+        String address = ip.getHostAddress();
+        System.out.println("Address : " + address);
+
+    }
+}
