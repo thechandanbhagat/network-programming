cb295a6362871b2a9d7f64d0c254130da22fb314
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-01-04 07:23:25 +0545

    interface lister added

---
+++ Chapter2/InterfaceLister.java
@@ -0,0 +1,15 @@
+package Chapter2;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class InterfaceLister {
+    public static void main(String[] args) throws SocketException {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface ni = interfaces.nextElement();
+            System.out.println("Interface Name: " + ni.getName());
+        }
+    }
+}
