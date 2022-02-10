28060d0be7724ab3c45b8e45b34a9e19eb944e4a
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-02-10 11:57:59 +0545

    updated the content reader from url and gitignore updated

--- Chapter3/ReadDataFromURL.java
+++ Chapter3/ReadDataFromURL.java
@@ -3,16 +3,24 @@ package Chapter3;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class ReadDataFromURL{
     public static void main(String[] args) {
         try {
-            URL u = new URL("https://chandanbhagat.com.np");
-            InputStream in = u.openStream();
+            
+            URL u = new URL("https://www.google.com");
+            URLConnection uc=u.openConnection();
+            uc.connect();
+            System.out.println(uc.getContentType());
+            System.out.println(uc.getHeaderField("Expires"));
+            System.out.println("\n\n");
+            InputStream in = uc.getInputStream();
             int c;
-            while ((c = in.read()) != -1) System.out.println(c);
+            while ((c = in.read()) != -1) System.out.print(Character.toChars(c));
             in.close();
-        } catch (IOException ex) {
+        }
+        catch (IOException ex) {
             System.err.println(ex);
         }
 
