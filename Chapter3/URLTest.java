f5327021c406f7bc206c8809a64b4a0304f05db3
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-12 09:59:19 +0545

    added the print of url object

--- Chapter3/URLTest.java
+++ Chapter3/URLTest.java
@@ -1,5 +1,3 @@
-package Chapter3;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -8,6 +6,8 @@ public class URLTest {
         try{
 
             URI u = new URI(null, "www.google.com", "/", null);
+            //if there is any parsing error then it throws the exception URISyntaxException
+            System.out.println(u.toString());
         }
         catch (URISyntaxException ex){
             System.out.println(ex.toString());
