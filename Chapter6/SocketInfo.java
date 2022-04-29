b966d7176fb5772b43abbf20d13f38c6ae9aa551
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-04-29 12:34:33 +0545

    socket info code updated and running

--- Chapter6/SocketInfo.java
+++ Chapter6/SocketInfo.java
@@ -5,20 +5,23 @@ import java.net.UnknownHostException;
 
 public class SocketInfo {
     public static void main(String[] args) {
-        for (String host : args) {
-            try {
-                Socket theSocket = new Socket(host, 80);
+        getInfo("www.google.com");
+    }
+
+    private static void getInfo(String host) {
+        try {
+            try (Socket theSocket = new Socket(host, 80)) {
                 System.out.println("Connected to " + theSocket.getInetAddress()
                         + " on port " + theSocket.getPort() + " from port "
                         + theSocket.getLocalPort() + " of "
                         + theSocket.getLocalAddress());
-            } catch (UnknownHostException ex) {
-                System.err.println("I can't find " + host);
-            } catch (SocketException ex) {
-                System.err.println("Could not connect to " + host);
-            } catch (IOException ex) {
-                System.err.println(ex);
             }
+        } catch (UnknownHostException ex) {
+            System.err.println("I can't find " + host);
+        } catch (SocketException ex) {
+            System.err.println("Could not connect to " + host);
+        } catch (IOException ex) {
+            System.err.println(ex);
         }
     }
 }
